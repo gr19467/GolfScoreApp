cbcb96a89d2744a0f2a442328668f423e9543840
Gaby Rodriguez <leapinlab@gmail.com>
2023-10-09 08:24:59 -0600

    finished homepage styling

---
+++ app.js
@@ -0,0 +1,15 @@
+let courses = getAvailableCourses();
+
+function getAvailableCourses() {
+    return fetch('https://golf-courses-api.herokuapp.com/courses/').then(
+      function (response) {
+        return response.json();
+      }
+    );
+}
+
+let courseOptionsHtml = '';
+courses.forEach((course) => {
+ courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`;
+});
+document.getElementById('course-select').innerHTML = courseOptionsHtml;
