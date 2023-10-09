06b822ef603ab3cbc28c0909441b900d908b9a8d
Gaby Rodriguez <leapinlab@gmail.com>
2023-10-09 09:18:24 -0600

    added courses and styling for dropdown

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
-let courses = getAvailableCourses();
+function onload(){
+    let courses = [
+    {
+      "id": 11819,
+      "name": "Thanksgiving Point Golf Course - Lehi, UT",
+      "url": "https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/course11819.json"
+    },
+    {
+      "id": 18300,
+      "name": "Fox Hollow Golf Course - American Fork, UT",
+      "url": "https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/course18300.json"
+    },
+    {
+      "id": 19002,
+      "name": "Spanish Oaks Golf Course - Spanish Fork, UT",
+      "url": "https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/course19002.json"
+    }
+  ];
 
-function getAvailableCourses() {
-    return fetch('https://golf-courses-api.herokuapp.com/courses/').then(
-      function (response) {
-        return response.json();
-      }
-    );
+  let courseOptionsHtml = '';
+  courses.forEach((course) => {
+    courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`;
+  });
+  document.getElementById("course-select").innerHTML = courseOptionsHtml;
 }
 
-let courseOptionsHtml = '';
-courses.forEach((course) => {
- courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`;
-});
-document.getElementById('course-select').innerHTML = courseOptionsHtml;
+function courseDropdown(){
+  dropdown = document.getElementById("course-select");
+  dropdown.style.display = "block";
+}
+
+function courseSelected(course){
+  //hide the dropdown
+
+  //change the selection box to say the course they selected
+
+  //light up the go button
+}
